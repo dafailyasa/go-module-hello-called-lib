b75ded73d54e63dbdd1ce23a7fbdd94a9ad7b796
dafailyasa <dafailyasa30@gmail.com>
2023-11-23 16:52:41 +0700

    called another module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dafailyasa/go-module-hello-called-lib
+
+go 1.19
+
+require github.com/dafailyasa/go-module-hello-lib v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/dafailyasa/go-module-hello-lib"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello("oke"))
+}
